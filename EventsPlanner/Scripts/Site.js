3ac4b620b6bd57adfbb01ab4c345cee7ffa0e9bc
Artem Grishanovich <g.artyom08@gmail.com>
2019-12-17 18:52:46 +0300

    finished course project

---
+++ EventsPlanner/Scripts/Site.js
@@ -0,0 +1,16 @@
+﻿$('#darkthene-checkbox').change(function () {
+    if (this.checked)
+    {
+        $('body').addClass('dark-theme');
+        $(".navbar").removeClass("navbar-default");
+        $(".navbar").addClass("navbar-inverse");
+    }
+    else
+    {
+        $('body').removeClass('dark-theme');
+        $(".navbar").removeClass("navbar-inverse");
+        $(".navbar").addClass("navbar-default");
+
+
+    }
+});
